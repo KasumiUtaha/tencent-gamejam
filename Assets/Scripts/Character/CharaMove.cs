6c6e94ef69fb9967a3314dadc2ff1a3b99cb7338
KasumiUtaha <1211180157@qq.com>
2024-10-19 22:02:29 +0800

    add DoTween and AudioSystem

---
+++ Assets/Scripts/Character/CharaMove.cs
@@ -0,0 +1,93 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaMove : MonoBehaviour
+{
+    public float jumpHeight;
+    public float moveSpeed;
+    public float moveSpeedAir;
+
+    public Transform groundDetector1;
+    public Transform groundDetector2;
+    public LayerMask groundLayer;
+    public float groundDetectDis;
+    [HideInInspector]
+    public float onPlaneVelocity = 0f;
+
+    [HideInInspector]
+    public bool canMove { get; private set; } = true;
+
+    private Rigidbody2D rb;
+    private Collider2D circleCollider;
+
+    [HideInInspector]
+    public bool onGround { get; private set; } = false;
+    [HideInInspector]
+    public int direction { get; private set; } = 1;
+    [HideInInspector]
+    public bool walking { get; private set; } = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PhysicsDetect();
+        //Debug.Log(rb.velocity);
+        //Debug.Log(canMove);
+        if(canMove && Input.GetButton("Jump"))
+        {
+            //Debug.Log(onGround);
+            //Debug.Log(rb.velocityY);
+            if(onGround && rb.velocityY <= 0.001f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, Mathf.Sqrt(-Physics2D.gravity.y * rb.gravityScale * 2 * jumpHeight));
+                //Debug.Log("Jump " + rb.velocityY);
+            }
+        }
+        //Debug.Log(rb.velocityY);
+        float inputVelocity = 0f;
+        if(canMove)
+        {
+            if(onGround)
+            {
+                inputVelocity = Input.GetAxisRaw("Horizontal") * moveSpeed;
+            }
+            else
+            {
+                inputVelocity = Input.GetAxisRaw("Horizontal") * moveSpeedAir;
+            }
+            direction =
+                direction > 0 ?
+                (inputVelocity > -0.01 ? 1 : -1) :
+                (inputVelocity > 0.01 ? 1 : -1) ;
+            walking = inputVelocity > 0.1f || inputVelocity < -0.1f;
+        }
+        rb.velocityX = inputVelocity + onPlaneVelocity;
+    }
+
+    private void PhysicsDetect()
+    {
+        RaycastHit2D hit1 = Physics2D.Raycast(groundDetector1.position, Vector2.down, groundDetectDis, groundLayer);
+        RaycastHit2D hit2 = Physics2D.Raycast(groundDetector2.position, Vector2.down, groundDetectDis, groundLayer);
+
+        if(hit1 || hit2)
+        {
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
+        }
+        //Debug.Log(onGround);
+    }
+}
---
+++ Assets/Scripts/GameFrame/Audio/AudioManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    private GameObject audioPlayerPrefab;
+
+    private void Awake()
+    {
+        if(instance)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            audioPlayerPrefab = ResourceManager.LoadPrefab("AudioPlayer");
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void Play(string name, float volume = 0.5f, float pitch = 1.0f)
+    {
+        var ap  = Instantiate(audioPlayerPrefab,transform).GetComponent<AudioPlayer>();
+        ap.audioSource.clip = ResourceManager.LoadAudio(name);
+        ap.audioSource.pitch = pitch;
+        if(!ap.audioSource.clip)
+        {
+            Debug.LogWarning(name + "");
+        }
+        ap.gameObject.name = name;
+        ap.audioSource.volume = volume;
+        ap.audioSource.Play();
+        Destroy(ap.gameObject, ap.audioSource.clip.length);
+    }
+    public AudioPlayer GetAudioPlayer(string name)
+    {
+        AudioPlayer ap = Instantiate(audioPlayerPrefab, transform).GetComponent<AudioPlayer>();
+        ap.audioSource.clip = ResourceManager.LoadAudio(name);
+        if (!ap.audioSource.clip)
+        {
+            Debug.LogWarning(name + "");
+        }
+        return ap;
+    }
+}
