252682e31ce664dd94674c378d3dcf8ee5b67e2b
unknown <zyj1456017580@163.com>
2024-10-20 22:02:26 +0800

    UI1.1

---
+++ Assets/Scripts/UI/Menu/MenuManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : UIManager
+{
+
+    [SerializeField]
+    private GameObject menuCanvas;
+    private bool timePause;
+    public void SetTimePause(bool flag)
+    {
+        timePause = flag;
+    }
+    protected override void Start()
+    {
+        base.Start();
+        MenuClose();
+        timePause = true;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Æô¶¯²Ëµ¥");
+            if (menuCanvas.activeInHierarchy)
+            {
+                OnClose();
+            }
+            else
+            {
+                OnOpen();
+            }
+        }
+    }
+    protected override void OnOpen()
+    {
+        base.OnOpen();
+        MenuOpen();
+    }
+
+    protected override void OnClose()
+    {
+        base.OnClose();
+        MenuClose();
+    }
+    
+    private void TimePause()
+    {
+        Time.timeScale = 0f;
+    }
+    private void TimeReturn()
+    {
+        Time.timeScale = 1f;
+    }
+    public void MenuOpen()
+    {
+        if (timePause)
+        {
+            TimePause();
+        }
+        menuCanvas.SetActive(true);
+    }
+    public void MenuClose()
+    {
+        TimeReturn();
+        menuCanvas.SetActive(false);
+    }
+}
