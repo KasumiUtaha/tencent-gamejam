3e6bfaee44632841b114a840aa760971aecc2fa2
Xiaxiaxia <3103841669@qq.com>
2024-10-28 19:20:05 +0800

    Optimize the dialogue system; Optimize the ice;

--- Assets/Scripts/Mechanism/ice.cs
+++ Assets/Scripts/Mechanism/ice.cs
@@ -9,6 +9,7 @@ public class Ice : MonoBehaviour
     public GameObject iceGround;
     public GameObject player;
     public Rigidbody2D rb;
+    public IceCollider iceCollider;
    
 
 
@@ -16,47 +17,54 @@ public class Ice : MonoBehaviour
     Vector3 icePosition;
     Vector3 playerPosition;
     Vector2 dir;
+    int onIceCount;
     public float speedX;
+    public float height;
+    float width;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        iceScale = iceGround.transform.lossyScale; 
+        iceScale = iceGround.transform.lossyScale;
+        width = iceScale.x;
         icePosition = iceGround.transform.position;
     }
     
     void Update()
     {
+        onIceCount = iceCollider.onIceCount;
+        Debug.Log(onIceCount);
         playerPosition = player.transform.position;
         isOnice();
     }
-
     public void isOnice()
     {
-        
-        if (playerPosition.y > icePosition.y && playerPosition.y < icePosition.y + 2f)
+        if (onIceCount > 0)
         {
-            if (icePosition.x + (iceScale.x / 2) >= playerPosition.x && icePosition.x - (iceScale.x / 2) <= playerPosition.x)
+            if (playerPosition.y > icePosition.y && playerPosition.y < icePosition.y + height)
             {
-                cM.onIce = true;
-                cM.canMove = false;
-                cM.rb.velocityX = speedX * cM.direction;
-            }
-            else
-            {
-                Debug.Log(cM.onIce);
-                if (cM.onIce)
+                if (icePosition.x + (iceScale.x / 2) >= playerPosition.x && icePosition.x - (iceScale.x / 2) <= playerPosition.x)
                 {
-                    cM.onIce = false;
-                    cM.canMove = true;
+                    cM.onIce = true;
+                    cM.canMove = false;
+                    cM.rb.velocityX = speedX * cM.direction;
+                }
+                else
+                {
+                    //Debug.Log(cM.onIce);
+                    if (cM.onIce)
+                    {
+                        cM.onIce = false;
+                        cM.canMove = true;
+                    }
                 }
             }
         }
     }
     void OnDrawGizmos()
     {
+        Vector3 ice = new Vector3(iceGround.transform.position.x, iceGround.transform.position.y + iceScale.y / 2,0);
         Gizmos.color = Color.red;
-
-        Gizmos.DrawWireCube(iceGround.transform.position, Vector3.one * 2f);
+        Gizmos.DrawWireCube(ice, new Vector3(width, height, 0));
     }
 }
